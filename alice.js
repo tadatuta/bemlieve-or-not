a8c04994f87b24f36535063115ec12c94c13b3e1
Vladimir Grinenko <i@tadatuta.com>
2019-08-19 10:21:01 +0300

    make zeit/now happy

--- alice.js
+++ alice.js
@@ -1,10 +1,10 @@
 const { json } = require('micro');
 const game = require('./lib/game');
 
-const helloText = 'Проверь свои знания про БЭМ! Я буду делать разные утверждения, а ты должен ответить, веришь ли ты им или нет.';
+const helloText = 'Проверьте свои знания про БЭМ! Я буду делать разные утверждения, а вы должны ответить, верите вы им или нет.';
 const invalidText = 'Выражайтесь, пожалуйста, понятнее!';
-const trueTriggers = ['верю', 'да', 'согласен', 'бэм'];
-const falseTriggers = ['не верю', 'нет', 'не согласен', 'не бэм'];
+const trueTriggers = ['верю', 'верно', 'да', 'согласен', 'бэм'];
+const falseTriggers = ['не верю', 'неверно', 'нет', 'не согласен', 'не бэм'];
 const validTriggers = trueTriggers.concat(falseTriggers);
 
 const db = {};
@@ -15,38 +15,39 @@ module.exports = async req => {
     const chat = db[session.session_id] || (db[session.session_id] = {});
 
     const command = request.command.toLowerCase();
+    console.log(`command`, command);
 
-    const response = {
+    const replay = {
         version,
         session,
         response: { end_session: false }
     };
 
     if (session.new || command === 'помощь' || command.includes('что ты умеешь')) {
-        const question = game.onStart();
-        chat.question = question;
-        response.text = helloText + ' ' + question;
+        const gameState = game.onStart();
+        chat.question = gameState;
+        replay.response.text = helloText + '\n' + gameState.question;
 
-        return response;
+        return replay;
     };
 
-    const isValid = validTriggers.includes(commond);
+    const isValid = validTriggers.includes(command);
     if (!isValid) {
 
-        const question = game.onStart();
-        chat.question = question;
-        response.text = [invalidText, helloText, question].join(' ');
+        const gameState = game.onStart();
+        chat.question = gameState;
+        replay.response.text = [invalidText, helloText, gameState.question].join('\n');
 
-        return response;
+        return replay;
     }
 
     const isCorrect = chat.question.isTrue ?
-        trueTriggers.includes(command) : falseTriggers.includes(commond);
+        trueTriggers.includes(command) : falseTriggers.includes(command);
 
-    const gameState = game.onAnswer(isCorrect);
+    const gameState = game.onAnswer(isCorrect, chat.question);
 
     chat.question = gameState.question;
-    response.text = gameState.message;
+    replay.response.text = gameState.message;
 
-    return response;
+    return replay;
 };
