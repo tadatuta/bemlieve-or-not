213e65b24c07900b7482474b1e0faac9aac1b5a9
Vladimir Grinenko <i@tadatuta.com>
2019-08-20 18:54:37 +0300

    Make it better

--- alice.js
+++ alice.js
@@ -3,40 +3,61 @@ const game = require('./lib/game');
 
 const helloText = 'Проверьте свои знания про БЭМ! Я буду делать разные утверждения, а вы должны ответить, верите вы им или нет.';
 const invalidText = 'Выражайтесь, пожалуйста, понятнее!';
-const trueTriggers = ['верю', 'верно', 'да', 'согласен', 'бэм'];
-const falseTriggers = ['не верю', 'неверно', 'нет', 'не согласен', 'не бэм'];
+
+const trueTriggers = ['верю', 'верно', 'правда', 'да', 'согласен', 'бэм', 'так и есть', 'конечно'];
+const falseTriggers = ['не верю', 'неправда', 'неверно', 'нет', 'не согласен', 'не бэм'];
 const validTriggers = trueTriggers.concat(falseTriggers);
 
 const db = {};
 
+function prepareTTS(text) {
+    return text
+        .replace(/CSS/g, 'си эс эс')
+        .replace(/JS/g, 'джэй эс');
+}
+
 module.exports = async req => {
     const { request, session, version } = await json(req);
-
-    const chat = db[session.session_id] || (db[session.session_id] = {});
-
-    const command = request.command.toLowerCase();
-    console.log(`command`, command);
-
     const replay = {
         version,
         session,
         response: { end_session: false }
     };
 
-    if (session.new || command === 'помощь' || command.includes('что ты умеешь')) {
-        const gameState = game.onStart();
-        chat.question = gameState;
-        replay.response.text = helloText + '\n' + gameState.question;
+    const command = request.command.toLowerCase();
+    console.log(`command`, command);
+
+    const chat = db[session.session_id] || (db[session.session_id] = {});
+    const isNewSession = session.new || command === 'помощь' || command.includes('что ты умеешь');
+
+    let gameState;
+    let responsePrefix = '';
+
+    function getReplay() {
+        chat.question = gameState.question;
+        replay.response.text = responsePrefix + gameState.message;
+
+        if (gameState.question.tts) {
+            replay.response.tts = gameState.question.tts;
+        } else {
+            const tts = prepareTTS(gameState.message);
+            if (tts !== gameState.message) {
+                replay.response.tts = responsePrefix + tts;
+            }
+        }
 
         return replay;
+    }
+
+    if (isNewSession) {
+        gameState = game.onAnswer();
+        responsePrefix = helloText + '\n';
+
+        return getReplay();
     };
 
-    const isValid = validTriggers.includes(command);
-    if (!isValid) {
-
-        const gameState = game.onStart();
-        chat.question = gameState;
-        replay.response.text = [invalidText, helloText, gameState.question].join('\n');
+    if (!validTriggers.includes(command)) {
+        replay.response.text = invalidText;
 
         return replay;
     }
@@ -44,10 +65,7 @@ module.exports = async req => {
     const isCorrect = chat.question.isTrue ?
         trueTriggers.includes(command) : falseTriggers.includes(command);
 
-    const gameState = game.onAnswer(isCorrect, chat.question);
+    gameState = game.onAnswer(isCorrect, chat.question);
 
-    chat.question = gameState.question;
-    replay.response.text = gameState.message;
-
-    return replay;
+    return getReplay();
 };
